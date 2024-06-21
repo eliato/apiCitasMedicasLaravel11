afba497ed7bcfcadbf2ee31d503c7b209e8e387e
Elias Torres <eliato_@hotmail.com>
2024-06-20 23:05:11 -0600

    adding AuthController

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
